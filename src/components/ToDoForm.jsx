2150a2f0d23034dc6d9ea6c5b07ad486b4204529
Patricia-Sigei <pattychela97@gmail.com>
2024-11-22 22:36:51 +0300

    allows users to add tasks and renders them as a list

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -8,15 +8,19 @@ function ToDoForm() {
     setTask (e.target.value);
   };
   const handleAddTask = (e) => {
-    setList (e.target.value);
-  };
-  const addTask = (e) =>{
     e.preventDefault ();
+    task.trim()? (setList ([...list , task]), setTask ('')) : null;
   };
+
   return (
     <form className='ToDoForm'>
-        <input type="text" className='ToDo-Input' placeholder='What is the Task Today?' onChange={handleInputChange}/>
+        <input type="text" className='ToDo-Input' value = {task} placeholder='What is the Task Today?' onChange={handleInputChange}/>
         <button onClick={handleAddTask} type='submit' className='ToD0-btn'>Add Task</button>
+        <ul>
+        {list.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
     </form>
   )
 }
