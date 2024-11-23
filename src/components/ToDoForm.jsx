cf50eccf8af5c4e7ac13f60094f1def76623f472
Patricia-Sigei <pattychela97@gmail.com>
2024-11-23 23:18:03 +0300

    adds functions to handle editing and updating new tasks

--- src/components/ToDoForm.jsx
+++ src/components/ToDoForm.jsx
@@ -1,9 +1,13 @@
 import React, { useState } from "react"
 import DeleteToDo from "./DeleteToDo";
+import EditToDoForm from "./EditToDoForm";
 
 function ToDoForm() {
   const [task, setTask] = useState ('');
   const [list, setList] = useState ([]);
+  const [editTask, setEditTask] = useState (null);
+  const [updateNewTask, setUpdateNewTask]= useState ('')
+
 
   const handleInputChange = (e) => {
     setTask (e.target.value);
@@ -16,6 +20,18 @@ function ToDoForm() {
     const newList = list.filter((_, i) => i !== index); 
     setList(newList);
   };
+  const handleEdit = (index) => {
+    setEditTask (index);
+    setUpdateNewTask (list[index]);
+  };
+  const handleUpdateTask = () => {
+    const updatedList = [...list];
+    updatedList [editTask] = updateNewTask
+    setList(updatedList);
+    setEditTask (null);
+    setUpdateNewTask('');
+  };
+  
 
   return (
     <form className='ToDoForm'>
